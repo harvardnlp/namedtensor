5715e8c23c72cb029706c1069d751b38f348b3cb
srush <srush@seas.harvard.edu>
2019-01-02 18:27:29 -0500

    Update __init__.py

--- namedtensor/__init__.py
+++ namedtensor/__init__.py
@@ -1,4 +1,4 @@
 from . import core
-from .core import NamedTensor, randn, ones, zeros, contract, assert_match, build
+from .core import NamedTensor, randn, ones, zeros, contract, assert_match, build, _im_init
 
 version = "0.0.1"
