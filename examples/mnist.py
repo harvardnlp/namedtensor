e85402e12efc6d8155f7e4c4de88d4bcce0615c4
Sasha Rush <srush@seas.harvard.edu>
2019-01-06 22:51:32 -0500

    Add doc and examples

--- examples/mnist.py
+++ examples/mnist.py
@@ -20,10 +20,10 @@ class Net(nn.Module):
             return F.max_pool2d(x, 2, 2)
         return (
             NamedTensor(x, ("b", "c", "h", "w"))
-            .op(self.conv1, h1="h", w1="w", c1="c")
+            .op(self.conv1,  c1="c", h1="h", w1="w")
             .op(F.relu)
             .op(pool, h1a="h1", w1a="w1")
-            .op(self.conv2, h2="h1a", w2="w1a", c2="c1")
+            .op(self.conv2, c2="c1", h2="h1a", w2="w1a")
             .op(pool, h2a="h2", w2a="w2")
             .stack(fc=("c2", "h2a", "w2a"))
             .op(self.fc1, fc2="fc")
