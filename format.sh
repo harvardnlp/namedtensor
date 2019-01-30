a0d3b7b8ce590fdd39f4c93a2e5fb1bf988689e2
srush <srush@seas.harvard.edu>
2019-01-29 23:01:08 -0500

    Update format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 black namedtensor/ --line-length=79
 black examples/ --line-length=79
-flake8 --ignore "N801, E203, E266, E501, W503" namedtensor
+flake8 --ignore "N801, E203, E266, E501, W503, N812" namedtensor
