38c430eb8f7b3c82e2003d471dde15b3043c0536
suzgunmirac <msuzgun@college.harvard.edu>
2019-01-30 20:34:14 -0500

    added the type function into core.py

--- namedtensor/core.py
+++ namedtensor/core.py
@@ -51,6 +51,11 @@ class NamedTensorBase:
         "The ordered dict of available dimensions."
         return self._schema.ordered_dict(self._tensor.size())
 
+    @property
+    def type(self):
+        "Return the type of the tensor"
+        return self._tensor.type()
+
     def __repr__(self):
         return "NamedTensor(\n\t%s,\n\t%s)" % (
             self._tensor,
