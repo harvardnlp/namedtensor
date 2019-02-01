aaa9d99aa4cb6f3c9c13b0c38bb74f599f340951
srush <srush@seas.harvard.edu>
2019-02-01 17:55:17 +0000

    .

--- namedtensor/torch_nn.py
+++ namedtensor/torch_nn.py
@@ -24,8 +24,13 @@ class _Update:
         return self
 
     def __call__(self, input):
-        updates = {} if "_updates" not in self.__dict__ else self._updates
-        return input.op(super(_Update, self).forward, **updates)
+        if "_spec" in self.__dict__:
+            input = input.transpose(self._input_order).contiguous()
+            updates = self._output_update
+            return input.op(super(_Update, self).forward, **updates)
+        else:
+            updates = {} if "_updates" not in self.__dict__ else self._updates
+            return input.op(super(_Update, self).forward, **updates)
 
 
 class _Flat:
@@ -39,10 +44,15 @@ class _Loss:
         return self
 
     def __call__(self, input, target):
-        assert "_reduced" in dir(self), "Must call 'reduce' first."
-
-        return input.reduce2(target, super(_Loss, self).forward, self._reduced)
-
+        if "_spec" in self.__dict__:
+            reduced = list(self._input_order)
+            if self._reduce != "none":
+                reduced = input._schema._names
+            input = input.transpose(self._input_order).contiguous()
+            return input.reduce2(target, super(_Loss, self).forward, reduced)
+        else:
+            assert "_reduced" in dir(self), "Must call 'reduce' first."
+            return input.reduce2(target, super(_Loss, self).forward, self._reduced)
 
 class _Augment:
     def augment(self, name):
@@ -50,11 +60,14 @@ class _Augment:
         return self
 
     def forward(self, input):
-        augment = (
-            "embedding" if "_augment" not in self.__dict__ else self._augment
-        )
-
-        return input.augment(super(_Augment, self).forward, augment)
+        if "_spec" in self.__dict__:
+            input = input.transpose(self._input_order).contiguous()
+            return input.augment(super(_Augment, self).forward, self._output_augment )
+        else:
+            augment = (
+                "embedding" if "_augment" not in self.__dict__ else self._augment
+            )
+            return input.augment(super(_Augment, self).forward, augment)
 
 
 _wrap = ["Dropout"]
@@ -68,32 +81,61 @@ Dropout.__doc__ = nn.Dropout.__doc__
 
 
 class Linear(_Update, nn.Linear):
-    pass
-
+    def spec(self, dim_in, name_out=None):
+        self._spec = True
+        self._input_order = (dim_in,)
+        self._output_update = {dim_in:
+            name_out if name_out else dim_in}
 
 class Conv1d(_Update, nn.Conv1d):
-    pass
-
+    def spec(self, dim_in, dim_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in, dim_conv)
+        self._output_update = {dim_in:
+            name_out if name_out else dim_in}
 
 class Conv2d(_Update, nn.Conv2d):
-    pass
+    def spec(self, dim_in, dims_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in,) + dims_conv
+        self._output_update =  {dim_in:
+            name_out if name_out else dim_in}
 
 
 class Conv3d(_Update, nn.Conv2d):
-    pass
+    def spec(self, dim_in, dims_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in,) + dims_conv
+        self._output_update =  {dim_in:
+            name_out if name_out else dim_in}
 
 
 class MaxPool1d(_Update, nn.MaxPool1d):
-    pass
+    def spec(self, dim_in, dim_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in, dim_conv)
+        self._output_update =  {dim_in:
+            name_out if name_out else dim_in}
+
+
 
 
 class MaxPool2d(_Update, nn.MaxPool2d):
-    pass
+    def spec(self, dim_in, dims_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in,) + dims_conv
+        self._output_update = {dim_in:
+            name_out if name_out else dim_in}
+
+
 
 
 class MaxPool3d(_Update, nn.MaxPool2d):
-    pass
-
+    def spec(self, dim_in, dims_conv, name_out):
+        self._spec = True
+        self._input_order = (dim_in,) + dims_conv
+        self._output_update =  {dim_in:
+            name_out if name_out else dim_in}
 
 _update = [
     "Linear",
@@ -116,12 +158,12 @@ MaxPool3d.__doc__ = nn.MaxPool3d.__doc__
 
 
 class CrossEntropyLoss(_Loss, nn.CrossEntropyLoss):
-    pass
-
+    def spec(self, dim_target):
+        self._input_order = (dim_target,)
 
 class NLLLoss(_Loss, nn.NLLLoss):
-    pass
-
+    def spec(self, dim_target):
+        self._input_order = (dim_target,)
 
 _loss = ["CrossEntropyLoss", "NLLLoss"]
 
@@ -130,7 +172,9 @@ NLLLoss.__doc__ = nn.NLLLoss.__doc__
 
 
 class Embedding(_Augment, nn.Embedding):
-    pass
+    def spec(self, dim_index, name_embedding):
+        self._input_order = (dim_index,)
+        self._output_augment = name_embedding
 
 
 _augment = ["Embedding"]
